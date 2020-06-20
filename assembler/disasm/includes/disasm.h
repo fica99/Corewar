2f54c89e960209e01203fdc0696850d67073c9b4
mmarti <mmarti@student.21-school.ru>
2020-06-20 15:18:48 +0300

    fix compile errors, norme errors

--- assembler/disasm/includes/disasm.h
+++ assembler/disasm/includes/disasm.h
@@ -28,15 +28,6 @@ typedef struct		s_buf
 	size_t			size;
 }					t_buf;
 
-typedef struct		s_champ
-{
-	unsigned		id;
-	unsigned char 	*name;
-	unsigned char	*comment;
-	size_t			exec_code_size;
-	char			*exec_code;
-}					t_champ;
-
 typedef struct	s_op
 {
 	char		*name;
@@ -47,140 +38,14 @@ typedef struct	s_op
 	uint8_t		t_dir_size;
 }				t_op;
 
-/*
-** Array
-*/
-
-t_op		g_op[16] = {
-		{
-				.name = "live",
-				.code = 0x01,
-				.args_num = 1,
-				.args_types_code = false,
-				.args_types = {T_DIR, 0, 0},
-				.t_dir_size = 4,
-		},
-		{
-				.name = "ld",
-				.code = 0x02,
-				.args_num = 2,
-				.args_types_code = true,
-				.args_types = {T_DIR | T_IND, T_REG, 0},
-				.t_dir_size = 4,
-		},
-		{
-				.name = "st",
-				.code = 0x03,
-				.args_num = 2,
-				.args_types_code = true,
-				.args_types = {T_REG, T_REG | T_IND, 0},
-				.t_dir_size = 4,
-		},
-		{
-				.name = "add",
-				.code = 0x04,
-				.args_num = 3,
-				.args_types_code = true,
-				.args_types = {T_REG, T_REG, T_REG},
-				.t_dir_size = 4,
-		},
-		{
-				.name = "sub",
-				.code = 0x05,
-				.args_num = 3,
-				.args_types_code = true,
-				.args_types = {T_REG, T_REG, T_REG},
-				.t_dir_size = 4,
-		},
-		{
-				.name = "and",
-				.code = 0x06,
-				.args_num = 3,
-				.args_types_code = true,
-				.args_types = {T_REG | T_DIR | T_IND, T_REG | T_DIR | T_IND, T_REG},
-				.t_dir_size = 4,
-		},
-		{
-				.name = "or",
-				.code = 0x07,
-				.args_num = 3,
-				.args_types_code = true,
-				.args_types = {T_REG | T_DIR | T_IND, T_REG | T_DIR | T_IND, T_REG},
-				.t_dir_size = 4,
-		},
-		{
-				.name = "xor",
-				.code = 0x08,
-				.args_num = 3,
-				.args_types_code = true,
-				.args_types = {T_REG | T_DIR | T_IND, T_REG | T_DIR | T_IND, T_REG},
-				.t_dir_size = 4,
-		},
-		{
-				.name = "zjmp",
-				.code = 0x09,
-				.args_num = 1,
-				.args_types_code = false,
-				.args_types = {T_DIR, 0, 0},
-				.t_dir_size = 2,
-		},
-		{
-				.name = "ldi",
-				.code = 0x0A,
-				.args_num = 3,
-				.args_types_code = true,
-				.args_types = {T_REG | T_DIR | T_IND, T_REG | T_DIR, T_REG},
-				.t_dir_size = 2,
-		},
-		{
-				.name = "sti",
-				.code = 0x0B,
-				.args_num = 3,
-				.args_types_code = true,
-				.args_types = {T_REG, T_REG | T_DIR | T_IND, T_REG | T_DIR},
-				.t_dir_size = 2,
-		},
-		{
-				.name = "fork",
-				.code = 0x0C,
-				.args_num = 1,
-				.args_types_code = false,
-				.args_types = {T_DIR, 0, 0},
-				.t_dir_size = 2,
-		},
-		{
-				.name = "lld",
-				.code = 0x0D,
-				.args_num = 2,
-				.args_types_code = true,
-				.args_types = {T_DIR | T_IND, T_REG, 0},
-				.t_dir_size = 4,
-		},
-		{
-				.name = "lldi",
-				.code = 0x0E,
-				.args_num = 3,
-				.args_types_code = true,
-				.args_types = {T_REG | T_DIR | T_IND, T_REG | T_DIR, T_REG},
-				.t_dir_size = 2,
-		},
-		{
-				.name = "lfork",
-				.code = 0x0F,
-				.args_num = 1,
-				.args_types_code = false,
-				.args_types = {T_DIR, 0, 0},
-				.t_dir_size = 2,
-		},
-		{
-				.name = "aff",
-				.code = 0x10,
-				.args_num = 1,
-				.args_types_code = true,
-				.args_types = {T_REG, 0, 0},
-				.t_dir_size = 4,
-		}
-};
+typedef struct		s_champ
+{
+	unsigned		id;
+	unsigned char 	*name;
+	unsigned char	*comment;
+	size_t			exec_code_size;
+	t_op			*exec_code;
+}					t_champ;
 
 t_champ	*validate_and_parse(char *fname);
 
--- assembler/disasm/src/parse_champ.c
+++ assembler/disasm/src/parse_champ.c
@@ -1,4 +1,5 @@
 #include "disasm.h"
+#include "disasm_op.h"
 
 unsigned char	*get_champ_name(uint8_t *bytes)
 {
@@ -26,6 +27,14 @@ size_t get_champ_exec_code_size(uint8_t *bytes)
 	return to_uint32(bytes);
 }
 
+t_op *get_champ_exec_code(uint8_t *bytes)
+{
+	bytes += MAGIC_HEADER_SIZE_BYTES + NULL_RANGE_BYTES +
+			EXEC_CODE_SIZE_BYTES + PROG_NAME_LENGTH + COMMENT_LENGTH;
+	(void)g_op;
+	return NULL;
+}
+
 t_champ 	*parse_champ(t_buf *buf)
 {
 	t_champ *champ;
@@ -36,6 +45,6 @@ t_champ 	*parse_champ(t_buf *buf)
 	champ->name = get_champ_name(buf->bytes);
 	champ->exec_code_size = get_champ_exec_code_size(buf->bytes);
 	champ->comment = get_champ_comment(buf->bytes);
-	//champ->exec_code = get_champ_exec_code(buf->bytes);	//todo
+	champ->exec_code = get_champ_exec_code(buf->bytes);	//todo
 	return (champ);
 }
