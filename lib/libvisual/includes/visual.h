5045c68dc2573735d4feab9cf7d644076cf5fa22
Filip Crnobrnja <aashara.c@gmail.com>
2020-06-25 00:55:13 +0300

    Add serialization
    * Serialize int
    * Serialize char
    * Serialize cell
    * Serialize player
    * Serialize arena

--- lib/libvisual/includes/visual.h
+++ lib/libvisual/includes/visual.h
@@ -2,6 +2,7 @@
 #define VISUAL_H
 
 #include <stdint.h>
+#include <stdio.h>
 #include "op.h"
 
 typedef enum
@@ -20,22 +21,31 @@ typedef struct		s_cell
 typedef struct		s_player
 {
 	uint8_t			id;
-	char			name[PROG_NAME_LENGT];
-	uint32_t		last_live;
-	uint16_t		lives_in_cur_period;
+	char			name[PROG_NAME_LENGTH];
+	int				last_live;
+	int				lives_in_cur_period;
 
 }					t_player;
 
 typedef struct		s_arena
 {
-	uint32_t		cycle;
-	int16_t			cycle_to_die;
-	uint16_t		cycle_delta;
-	uint16_t		nbr_live;
-	uint16_t		max_checks;
+	int				cycle;
+	int				cycle_to_die;
+	int				cycle_delta;
+	int				nbr_live;
+	int				max_checks;
 	t_player		players[MAX_PLAYERS];
 	t_cell			arena[MEM_SIZE];
 
 }					t_arena;
 
+
+/*
+**	serialize.c
+*/
+unsigned char	*serialize_int(unsigned char *buffer, int value);
+unsigned char	*serialize_char(unsigned char *buffer, char value);
+unsigned char	*serialize_cell(unsigned char *buffer, t_cell value);
+unsigned char	*serialize_player(unsigned char *buffer, t_player value);
+unsigned char	*serialize_arena(unsigned char *buffer, t_arena value);
 #endif
