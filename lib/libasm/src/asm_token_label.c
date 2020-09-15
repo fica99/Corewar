ea8fd991a9896fe30e347fef2a963aa484ab1f1a
Guy Grimes <ggrimes@il-j5.msk.21-school.ru>
2020-09-15 20:40:10 +0300

    Lexical analysis of tags  #35 * Added getting tag tokens * Norminette * Testing labels

---
+++ lib/libasm/src/asm_token_label.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   asm_token_label.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggrimes <ggrimes@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/15 19:35:08 by ggrimes           #+#    #+#             */
+/*   Updated: 2020/09/15 20:19:03 by ggrimes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libasm.h"
+
+int				asm_check_label(t_asm_string *asm_str)
+{
+	size_t	i;
+
+	i = asm_str->index;
+	while (asm_str->str[i])
+	{
+		if (asm_str->str[i] == LABEL_CHAR)
+			break ;
+		if (!ft_strchr(LABEL_CHARS, asm_str->str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+t_asm_token		*asm_token_label(t_asm_string *asm_str)
+{
+	t_asm_token	*token;
+	size_t		i;
+	size_t		delta;
+	char		*label;
+
+	token = asm_new_token(TT_LABEL);
+	i = asm_str->index;
+	while (asm_str->str[i] != LABEL_CHAR)
+	{
+		i++;
+		continue ;
+	}
+	delta = i - asm_str->index;
+	label = ft_strnew(delta);
+	ft_memcpy(label, asm_str->str + asm_str->index, delta);
+	token->data = (void *)(label);
+	token->size = delta;
+	asm_str->index = i + 1;
+	asm_str->char_num += i + 1;
+	return (token);
+}
