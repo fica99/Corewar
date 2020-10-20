1cd3e2f2f6af2739a7a29567a19ab0a2c3d2d84f
Guy Grimes <ggrimes@il-j5.msk.21-school.ru>
2020-10-20 21:11:14 +0300

    Added a General function for parsing operation arguments #54 * Added a stub function for the T_REG type argument * Added a stub function for the T_DIR type argument * Added a stub function for the T_IND type argument

--- lib/libasm/includes/libasm.h
+++ lib/libasm/includes/libasm.h
@@ -6,7 +6,7 @@
 /*   By: ggrimes <ggrimes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/25 20:22:41 by aashara-          #+#    #+#             */
-/*   Updated: 2020/10/20 20:45:44 by ggrimes          ###   ########.fr       */
+/*   Updated: 2020/10/20 21:08:13 by ggrimes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -156,6 +156,7 @@ typedef struct	s_asm_pars_prms
 	t_hash			**opers_hash;
 	int				exec_code_size;
 	int				args_mask;
+	char			args_size;
 	char			*error;
 	t_asm_labels	*labels;
 	size_t			line_num;
@@ -325,6 +326,12 @@ int				asm_direct_code_additional(int code);
 int				asm_pars_oper(t_asm_token **token,
 	t_asm_bin_data *bin_data, t_asm_pars_prms *prms);
 int 			asm_pars_arg(t_asm_token **token,
+	t_asm_bin_data *bin_data, t_asm_pars_prms *prms, char arg_index);
+void			asm_pars_reg(t_asm_token **token,
+	t_asm_bin_data *bin_data, t_asm_pars_prms *prms);
+void			asm_pars_dir(t_asm_token **token,
+	t_asm_bin_data *bin_data, t_asm_pars_prms *prms);
+void			asm_pars_ind(t_asm_token **token,
 	t_asm_bin_data *bin_data, t_asm_pars_prms *prms);
 /*
 ** asm_pars_labels.c
--- lib/libasm/src/asm_pars_opers.c
+++ lib/libasm/src/asm_pars_opers.c
@@ -6,20 +6,20 @@
 /*   By: ggrimes <ggrimes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/11 19:12:00 by ggrimes           #+#    #+#             */
-/*   Updated: 2020/10/20 20:46:25 by ggrimes          ###   ########.fr       */
+/*   Updated: 2020/10/20 21:10:45 by ggrimes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libasm.h"
 
-int	asm_direct_code_additional(int code)
+int		asm_direct_code_additional(int code)
 {
 	code = ~code;
 	code += 1;
 	return (code);
 }
 
-int	asm_pars_opers(t_asm_token **token,
+int		asm_pars_opers(t_asm_token **token,
 	t_asm_bin_data *bin_data, t_asm_pars_prms *prms)
 {
 	while (1)
@@ -39,7 +39,7 @@ int	asm_pars_opers(t_asm_token **token,
 	return (1);
 }
 
-int	asm_pars_oper(t_asm_token **token,
+int		asm_pars_oper(t_asm_token **token,
 	t_asm_bin_data *bin_data, t_asm_pars_prms *prms)
 {
 	t_asm_oper	*oper;
@@ -50,15 +50,68 @@ int	asm_pars_oper(t_asm_token **token,
 	bin_data->add(bin_data, (int)oper->code, 2);
 	prms->exec_code_size += 1;
 	prms->args_mask = oper->args_mask;
+	prms->args_size = 3;
 	(*token) = (*token)->next;
-	return (asm_pars_arg(token, bin_data, prms));
+	return (asm_pars_arg(token, bin_data, prms, 1));
 }
 
-int asm_pars_arg(t_asm_token **token,
+int		asm_pars_arg(t_asm_token **token,
+	t_asm_bin_data *bin_data, t_asm_pars_prms *prms, char arg_index)
+{
+	(void)bin_data;
+	char	offset;
+
+	if (arg_index <= 0)
+		return (asm_parser_error(*token, (*token)->type, prms, 0));
+	while (asm_skip_token(token, TT_SEP))
+		;
+	offset = arg_index - 1;
+	if ((prms->args_mask >> offset) & ARG_1_REG)
+		asm_pars_reg(token, bin_data, prms);
+	else if (((prms->args_mask >> offset) & ARG_1_DIR))
+		asm_pars_dir(token, bin_data, prms);
+	else if (((prms->args_mask >> offset) & ARF_1_IND))
+		asm_pars_ind(token, bin_data, prms);
+	else
+	{
+		while (asm_skip_token(token, TT_SEP))
+		;
+		if (!asm_check_nl(token, prms))
+			return (asm_parser_error(*token, TT_NEWLINE, prms, 0));
+		return (1);
+	}
+	return (asm_pars_arg(token, bin_data, prms, ++arg_index));
+}
+
+void	asm_pars_reg(t_asm_token **token,
 	t_asm_bin_data *bin_data, t_asm_pars_prms *prms)
 {
-	(void)token;
 	(void)bin_data;
 	(void)prms;
-	return (1);
+	while (asm_skip_token(token, TT_SEP))
+		;
+	(*token) = (*token)->next;
+	return ;
+}
+
+void	asm_pars_dir(t_asm_token **token,
+	t_asm_bin_data *bin_data, t_asm_pars_prms *prms)
+{
+	(void)bin_data;
+	(void)prms;
+	while (asm_skip_token(token, TT_SEP))
+		;
+	(*token) = (*token)->next;
+	return ;
+}
+
+void	asm_pars_ind(t_asm_token **token,
+	t_asm_bin_data *bin_data, t_asm_pars_prms *prms)
+{
+	(void)bin_data;
+	(void)prms;
+	while (asm_skip_token(token, TT_SEP))
+		;
+	(*token) = (*token)->next;
+	return ;
 }
